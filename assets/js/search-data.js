0ab40ef495f2547e41bc1dfdb368c92708044957
camhogan <53248181+camhogan@users.noreply.github.com>
2025-06-19 18:52:24 +0000

    Deploying to gh-pages from @ camhogan/camhogan.github.io@7148430f0650305b951432960d078b3f42043bd6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,11 +413,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/statistical_foundations_DS/";
-            },},{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
